edb557b690ea4c34fe7402e81e46bdcbe0f40c36
Danila Isaichev <danila.isaichev@mail.ru>
2023-02-17 00:08:43 +0300

    Now you can set hostname of db

--- schedule_db_data.go
+++ schedule_db_data.go
@@ -342,9 +342,9 @@ func scan_prepare(src interface{}) (prepared_bytes []byte, err error) {
 	return bdata, nil
 }
 
-func DB_connection(db_name string, username string, password string, port string) (db_conn *sql.DB, err error) {
+func DB_connection(hostname string, db_name string, username string, password string, port string) (db_conn *sql.DB, err error) {
 
-	connection_string := "host=localhost port=" + port + " user=" + username + " password=" + password + " dbname=" + db_name + " sslmode=disable"
+	connection_string := "host=" + hostname + "port=" + port + " user=" + username + " password=" + password + " dbname=" + db_name + " sslmode=disable"
 
 	db, err := sql.Open("postgres", connection_string)
 	if err != nil {
