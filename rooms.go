4b750d3478f3b01ef96875f0a8d6b84c461ac930
Исайчев Данила Олегович <danila@Laptop-ASUS-DI>
2023-06-16 03:03:51 +0300

    changed room struct

--- rooms.go
+++ rooms.go
@@ -9,13 +9,13 @@ import (
 type Room struct {
 	Id    int    `json:"id"`
 	Name  string `json:"name"`
-	Wing  *int   `json:"wing"`
-	Floor *int   `json:"floor"`
+	Wing  int   `json:"wing"`
+	Floor int   `json:"floor"`
 }
 
 type Rooms []Room
 
-// Сканер массива учителей
+// Сканер массива кабинетов
 func (r *Rooms) Scan(src interface{}) (err error) {
 
 	// Приведение полученных данных к корректному виду (массив байтов без служебных символов)
@@ -81,20 +81,22 @@ func (r *Rooms) scan_rooms(src []byte) (err error) {
 
 		if room_map["wing"] != nil {
 			if val, ok := room_map["wing"].(float64); ok {
-				room.Wing = new(int)
-				*room.Wing = int(val)
+				room.Wing = int(val)
 			} else {
 				return errors.New("couldn't convert 'wing' to int")
 			}
+		} else {
+			room.Wing = 0
 		}
 
 		if room_map["floor"] != nil {
 			if val, ok := room_map["floor"].(float64); ok {
-				room.Floor = new(int)
-				*room.Floor = int(val)
+				room.Floor = int(val)
 			} else {
 				return errors.New("couldn't convert 'floor' to int")
 			}
+		} else {
+			room.Floor = 0
 		}
 
 		rooms_array = append(rooms_array, room)
@@ -105,81 +107,61 @@ func (r *Rooms) scan_rooms(src []byte) (err error) {
 	return nil
 }
 
-func UnmarshalRoom(src interface{}) (Room, string, error) {
+func UnmarshalRoom(src interface{}) (Rooms, error) {
 
 	// Приведение полученных данных к корректному виду (массив байтов без служебных символов)
 	byte_str, err := scan_prepare(src)
 	if err != nil {
-		return Room{}, "", err
+		return Rooms{}, err
 	}
 
 	// Объявляем карту с строчным ключём и значением в виде интерфейса
-	var room_map map[string]interface{}
+	var room_maps []map[string]interface{}
 
 	// Записываем значения в карту
-	err = json.Unmarshal(byte_str, &room_map)
+	err = json.Unmarshal(byte_str, &room_maps)
 	if err != nil {
-		return Room{}, "", err
+		return Rooms{}, err
 	}
 
-	r := Room{}
-	a := ""
+	r := Rooms{}
 
-	if room_map["action"] != nil {
-		if val, ok := room_map["action"].(string); ok {
-			switch {
-			case val == "save":
-				a = "save"
-			case val == "delete":
-				a = "delete"
-			default:
-				return Room{}, "", errors.New("unknown action")
-			}
-		} else {
-			return Room{}, "", errors.New("couldn't convert action to string")
-		}
-	} else {
-		return Room{}, "", errors.New("no action found")
-	}
+	for _, room_map := range room_maps {
 
-	if room_map["room"] != nil {
-		if val, ok := room_map["room"].(map[string]interface{}); ok {
+		room := Room{}
 
-			room := val
-
-			if room["name"] != nil {
-				if val, ok := room["name"].(string); ok {
-					r.Name = val
-				} else {
-					return Room{}, "", errors.New("couldn't convert rooms's name to string")
-				}
+		if room_map["name"] != nil {
+			if val, ok := room_map["name"].(string); ok {
+				room.Name = val
 			} else {
-				return Room{}, "", errors.New("no rooms's name found")
-			}
-
-			if room["wing"] != nil {
-				if val, ok := room["wing"].(float64); ok {
-					r.Wing = new(int)
-					*(r.Wing) = int(val)
-				} else {
-					return Room{}, "", errors.New("couldn't convert room's wing to int")
-				}
-			}
-
-			if room["floor"] != nil {
-				if val, ok := room["floor"].(float64); ok {
-					r.Floor = new(int)
-					*(r.Floor) = int(val)
-				} else {
-					return Room{}, "", errors.New("couldn't convert room's floor to string")
-				}
+				return Rooms{}, errors.New("couldn't convert rooms's name to string")
 			}
 		} else {
-			return Room{}, "", errors.New("couldn't convert room's struct to map[string]interface{}")
+			return Rooms{}, errors.New("no rooms's name found")
 		}
-	} else {
-		return Room{}, "", errors.New("no 'room' found")
+
+		if room_map["wing"] != nil {
+			if val, ok := room_map["wing"].(float64); ok {
+				room.Wing = int(val)
+			} else {
+				return Rooms{}, errors.New("couldn't convert room's wing to int")
+			}
+		} else {
+			room.Wing = 0
+		}
+
+		if room_map["floor"] != nil {
+			if val, ok := room_map["floor"].(float64); ok {
+				room.Floor = int(val)
+			} else {
+				return Rooms{}, errors.New("couldn't convert room's floor to string")
+			}
+		} else {
+			room.Floor = 0
+		}
+
+		r = append(r, room)
 	}
 
-	return r, a, nil
+	return r, nil
 }
