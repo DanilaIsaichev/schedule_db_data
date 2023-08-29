b14f2fb0dab8e665c009f4ac9ea55ebf948b84bf
Danila Isaichev <danila.isaichev@mail.ru>
2023-08-29 20:07:03 +0300

    updated

--- get.go
+++ get.go
@@ -183,7 +183,7 @@ func Get_changed_schedule(start time.Time, parallel int) (Days, error) {
 	days := Days{}
 
 	// Запрашиваем расписание по дате
-	err = db.QueryRow("SELECT data FROM schedule WHERE is_base = False AND start = " + start.Format("02.01.2006") + " AND parallel = " + fmt.Sprint(parallel) + ";").Scan(&days)
+	err = db.QueryRow("SELECT data FROM schedule WHERE is_base = False AND start = " + start.Format("2006-01-02") + " AND parallel = " + fmt.Sprint(parallel) + ";").Scan(&days)
 	switch {
 	case err == sql.ErrNoRows:
 
@@ -375,7 +375,7 @@ func Get_editor_data(req Request) (Response, error) {
 			return Response{}, err
 		}
 	} else {
-		err = db.QueryRow("SELECT data FROM schedule WHERE is_base = False AND start = " + req.Start.Format("02.01.2006") + " AND parallel = " + fmt.Sprint(req.Parallel) + ";").Scan(&days)
+		err = db.QueryRow("SELECT data FROM schedule WHERE is_base = False AND start = " + req.Start.Format("2006-01-02") + " AND parallel = " + fmt.Sprint(req.Parallel) + ";").Scan(&days)
 		if err == sql.ErrNoRows {
 			err = db.QueryRow("SELECT data FROM schedule WHERE is_base = True AND year = " + fmt.Sprint(req.Year) + " AND parallel = " + fmt.Sprint(req.Parallel) + ";").Scan(&days)
 			if err != nil {
@@ -471,7 +471,7 @@ func Get_generator_data(req Request) (Generator_response, error) {
 			return Generator_response{}, err
 		}
 	} else {
-		err = db.QueryRow("SELECT data FROM schedule WHERE is_base = False AND start = " + req.Start.Format("02.01.2006") + " AND parallel = " + fmt.Sprint(req.Parallel) + ";").Scan(&days)
+		err = db.QueryRow("SELECT data FROM schedule WHERE is_base = False AND start = " + req.Start.Format("2006-01-02") + " AND parallel = " + fmt.Sprint(req.Parallel) + ";").Scan(&days)
 		if err == sql.ErrNoRows {
 			err = db.QueryRow("SELECT data FROM schedule WHERE is_base = True AND year = " + fmt.Sprint(req.Year) + " AND parallel = " + fmt.Sprint(req.Parallel) + ";").Scan(&days)
 			if err != nil {
